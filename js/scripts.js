87f198c3ad33060069f2b802224957f74f98f6aa
Mizz <123146044+MrMizzles@users.noreply.github.com>
2023-04-13 14:55:13 -0400

    Add files via upload
    Uploading the files manually because I am not sure how this happened but the GitHub Desktop and my VS code are not communicating and it is not committing any changes I save or commit to the desktop.  The changes made are to the js page where I added an array of three Pokemon.

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,6 @@
 /* Use comments often as it helps everyone, including yourself! */
-alert('Hello world!');
-let favoriteFood = 'favefood';
-document.write('crispy tofu and brown rice');
+let pokemonList = [
+    {name:  "Bulbasaur", height: 2, weight: 15.2, type: ['grass', 'poison']},
+    {name: "Charzard", height: 5, weight: 199.5, type: ['fire', 'flying']},
+    {name: "Mewtwo", height: 6, weight: 269, type: ['physhic']}
+];
